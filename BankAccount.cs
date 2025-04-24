a9f31509c955022dc2122de2b7d1925dfab28651
Pokouser <165961921+Pokouser@users.noreply.github.com>
2025-04-24 16:17:35 +0300

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName => m_customerName;
+        public double Balance => m_balance;
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+            }
+            if (amount < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+            }
+            m_balance -= amount; // Добавлено списание суммы
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount; // Исправлено с -= на +=
+        }
+    }
+}
---
+++ BankAccountTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+using System;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act
+            account.Debit(debitAmount);
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act and assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() =>
+            account.Debit(debitAmount));
+        }
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        [TestMethod]
+        public void Credit_witchZeroAmount_ShouldNotChangeBalace()
+        {
+            double beginningBalance = 100.0;
+            double creditAmount = 0.0;
+            BankAccount account = new BankAccount("Test user", beginningBalance);
+
+            account.Credit(creditAmount);
+
+            Assert.AreEqual(beginningBalance, account.Balance, 0.001, "balance should not change when credit zero amount");
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsNegative_ShouldThrowCorrectExceptionMessage()
+        {
+            double debitAmount = -50.0;
+            BankAccount account = new BankAccount("Test User", 100.0);
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                return;
+            }
+
+            Assert.Fail("Expected exception was not thrown");
+        }
+
+        [TestMethod]
+        public void Debit_WithFullBalanceAmount_ShouldSetBalanceToZero()
+        {
+
+            double beginningBalance = 100.0;
+            BankAccount account = new BankAccount("Test User", beginningBalance);
+
+
+            account.Debit(beginningBalance);
+
+            Assert.AreEqual(0.0, account.Balance, 10000,
+                "Balance should be zero after debiting full amount");
+        }
+    }
+}
